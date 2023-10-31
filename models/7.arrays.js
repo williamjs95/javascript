5e737557850899ed4e26de50387648f5ad726e19
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-10-31 16:47:40 -0300

    fun js

---
+++ models/7.arrays.js
@@ -0,0 +1,17 @@
+const animals = [
+    'Lion',
+    'Monkey',
+    {
+        name: 'Cat',
+        age: 3,
+    }
+]
+
+console.log(animals)
+
+console.log(animals.length)
+
+console.log(animals[0])
+
+console.log(animals[2].name)
+
