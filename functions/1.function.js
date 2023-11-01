dd5531470b78aa1694ef96e27b0302c45ee6aa7d
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-10-31 21:03:20 -0300

    function

---
+++ functions/1.function.js
@@ -0,0 +1,14 @@
+// function statement/declaration
+function createPhrases(){
+    console.log('Estudar  é muito bom')
+    console.log('Paciência e persistência')
+    console.log('Revisão é mãe do aprendizado')    
+}
+
+// execute, run, call, invoke
+createPhrases()
+createPhrases()
+createPhrases()
+createPhrases()
+
+console.log('fim  do programa')
---
+++ functions/2.function.js
@@ -0,0 +1,7 @@
+// function expression
+// anonymous function
+const sum = function(number1, number2){ // parameters
+    console.log (number1 + number2)
+}
+
+sum(2, 3) // arguments
