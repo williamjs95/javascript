ddf1fd54734c066a7c7de5343ceb1edaa3a4d9b1
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-10-31 21:22:08 -0300

    js

--- functions/2.function.js
+++ functions/2.function.js
@@ -1,7 +1,14 @@
 // function expression
 // anonymous function
 const sum = function(number1, number2){ // parameters
-    console.log (number1 + number2)
+    total = number1 + number2
+    return total
 }
 
-sum(2, 3) // arguments
+let number1 = 22
+let number2 = 34
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 2 é ${number2}`)
+console.log(`A soma é igual a ${sum(number1, number2)}`)
+console.log(total)
