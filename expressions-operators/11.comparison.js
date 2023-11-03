83ae87afbb2d4586971a4661c45a4a2989a05b0d
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-11-03 09:39:33 -0300

    ja

--- expressions-operators/11.comparison.js
+++ expressions-operators/11.comparison.js
@@ -7,10 +7,37 @@ let two = 2;
 console.log(two == 1);
 console.log(one == '1');
 
-console.log('   ')
+// estritamente igual a 
+console.log(one === '1');
+console.log(one === 1);
+
+
+console.log('');
+
 
 // diferente de
 console.log(one != two);
 console.log(one != 1);
 console.log(one != '1')
 
+// estritamente diferente de 
+console.log(two !== '2');
+console.log(two !== 2);
+
+
+console.log('');
+
+// maior que 
+console.log(one > two);
+
+// maior igual a 
+console.log(1 >= 1);
+console.log(two >= 1);
+
+// menor que
+console.log(one < two);
+
+// menor igual a
+console.log(one <= two);
+console.log(one <= 1);
+console.log(one <= 0);
