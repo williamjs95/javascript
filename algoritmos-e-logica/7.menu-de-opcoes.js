fa4c15a9337f9d5e3caddff63234320c1c4b9327
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-11-06 10:57:22 -0300

    menu js

---
+++ algoritmos-e-logica/7.menu-de-opcoes.js
@@ -0,0 +1,31 @@
+let option;
+let items = [];
+
+while(option != 3){
+
+    let option = Number(prompt(`
+    Olá uruário! Digite o número da opção desejada:
+    
+    1. Cadastrar um item na lista
+    2. Mostrar intens cadastrados
+    3. Sair do programa
+    `));
+
+if (option == 1){
+    let item = prompt('Digite o nome do item:');
+    items.push(item);
+} 
+
+else if (option == 2){
+
+    if (items.length == 0){
+        alert("Não existem intens cadastrados.");
+    } else{
+        alert(items);
+    }
+} else{
+    alert('Tchau');
+}
+
+console.log(option, items);
+}
