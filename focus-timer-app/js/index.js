00af27e923e48108f50a34efebdfdfae16f98104
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-11-16 03:32:41 -0300

    timerfocus refactory

--- focus-timer-app/js/index.js
+++ focus-timer-app/js/index.js
@@ -1,47 +1,23 @@
+import resetControls from './controls.js';
+import { Timer } from './timer.js';
+
 const buttonPlay = document.querySelector('.play');
 const buttonPause = document.querySelector('.pause');
 const buttonStop = document.querySelector('.stop');
 const buttonSet = document.querySelector('.set');
 const buttonSoundOn = document.querySelector('.sound-on');
 const buttonSoundOff = document.querySelector('.sound-off');
-let minutes;
 const minutesDisplay = document.querySelector('.minutes');
 const secondsDisplay = document.querySelector('.seconds');
+let minutes = Number(minutesDisplay.textContent);
+let timerTimeOut;
 
-function resetControls() {
-    buttonPlay.classList.remove('hide');
-    buttonPause.classList.add('hide');
-    buttonSet.classList.remove('hide');
-    buttonStop.classList.add('hide');
-}
-
-function updateTimerDisplay(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, '0');
-    secondsDisplay.textContent = String(seconds).padStart(2, '0');
-}
-
-function countdown() {
-    setTimeout(function() {
-        let seconds = Number(secondsDisplay.textContent);
-        let minutes = Number(minutesDisplay.textContent);
-
-        updateTimerDisplay(minutes, 0);
-
-        if (minutes <= 0) {
-            resetControls();
-            return;
-        }
-
-        if (seconds <= 0) {
-            seconds = 2;
-            --minutes;
-        }
-
-        updateTimerDisplay(minutes, String(seconds - 1));
-
-        countdown();
-    }, 1000)
-}
+const timer = Timer({
+    minutesDisplay,
+    secondsDisplay,
+    timerTimeOut,
+    resetControls,
+});
 
 buttonPlay.addEventListener('click', function() {
     buttonPlay.classList.add('hide');
@@ -49,16 +25,18 @@ buttonPlay.addEventListener('click', function() {
     buttonSet.classList.add('hide');
     buttonStop.classList.remove('hide');
 
-    countdown();
+    timer.countdown();
 })
 
 buttonPause.addEventListener('click', function() {
     buttonPause.classList.add('hide');
     buttonPlay.classList.remove('hide');
+    clearTimeout(timerTimeOut);
 })
 
 buttonStop.addEventListener('click', function() {
     resetControls();
+    timer.resetTimer();
 })
 
 buttonSoundOff.addEventListener('click', function() {
@@ -72,6 +50,12 @@ buttonSoundOn.addEventListener('click', function() {
 })
 
 buttonSet.addEventListener('click', function() {
-    minutes = prompt('Quantos minutos?');
+    let newMinutes = prompt('Quantos minutos?');
+    if(!newMinutes) {
+        timer.resetTimer();
+        return;
+    }
+
+    minutes = newMinutes;
     updateTimerDisplay(minutes, 0);
 })
