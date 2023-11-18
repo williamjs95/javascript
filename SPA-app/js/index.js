108a7fd03f80ae252cebf5e847f040dc606923a7
William Silva <129805046+williamjs95@users.noreply.github.com>
2023-11-18 04:02:49 -0300

    spa end

--- SPA-app/js/index.js
+++ SPA-app/js/index.js
@@ -1,34 +1,12 @@
-const routes = {
-    "/": "/SPA-app/pages/home.html",
-    "/about": "/SPA-app/pages/about.html",
-    "/contact": "/SPA-app/pages/contact.html",
-    404: "/SPA-app/pages/404.html",
-}
+import { Router } from './router.js';
 
-function route(event) {
-    event = event || window.event;
+const router = new Router();
+router.add("/", "/pages/home.html");
+router.add("/about", "/pages/about.html");
+router.add("/contact", "/pages/contact.html");
+router.add(404, "/pages/404.html");
 
-    event.preventDefault();
+router.handle();
 
-    window.history.pushState({}, "", event.target.href)
-
-    handle();
-}
-
-function handle() {
-    const { pathname } = window.location;
-    const route = routes[pathname] || routes[404];
-
-    fetch(route)
-    .then(data => data.text())
-    .then(html => {
-        document.querySelector('#app').innerHTML = html;
-    });
-    
-    console.log(route);
-}
-
-handle();
-
-window.onpopstate = () => handle();
-window.route = () => route();
+window.onpopstate = () => router.handle();
+window.route = () => router.route();
